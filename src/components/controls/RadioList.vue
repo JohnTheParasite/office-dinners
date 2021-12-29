d163bf55f7dcdddd3fa89ceb66686857f62d8859
moonvvell <moonvvell@ukr.net>
2021-12-29 17:06:26 +0100

    Fix for radio list

--- src/components/controls/RadioList.vue
+++ src/components/controls/RadioList.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="input radio-list" :class="{ invalid: hasError, readonly: readonly, horizontal: orientation !== 'vertical' }">
-    <span class="input-label" v-show="hasLabel" v-html="getLabel"></span>
+  <div :class="{ invalid: hasError, readonly: readonly, horizontal: orientation !== 'vertical' }" class="input radio-list">
+    <span v-show="hasLabel" class="input-label" v-html="getLabel"></span>
     <span class="input-block">
-      <label v-for="option in options" :key="option" class="option" :for="generateId(option)">
-        <input type="radio" :disabled="readonly" @change="change" :id="generateId(option)" :name="name" v-model="value" :value="option.value" />
+      <label v-for="option in options" :key="option.value" :for="generateId(option)" class="option">
+        <input :id="generateId(option)" v-model="value" :disabled="readonly" :name="name" :value="option.value" type="radio" @change="change" />
         <span class="radio-control">
           <span class="radio-border"></span>
           <span class="radio-checked"></span>
@@ -34,77 +34,75 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 @import "../../scss/components/color";
 
 .radio-list {
-  > label {
-    .input-block {
-      flex-direction: column;
-      align-items: flex-start;
+  .input-block {
+    flex-direction: column;
+    align-items: flex-start;
 
-      .option {
-        display: flex;
-        align-items: center;
-        width: 100%;
+    .option {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      cursor: pointer;
+      margin-bottom: 5px;
+      font-size: 12px;
+
+      .radio-control {
+        width: 18px;
+        height: 18px;
+        position: relative;
+        display: block;
+        border-radius: 50%;
         cursor: pointer;
-        margin-bottom: 5px;
-        font-size: 12px;
+        flex-shrink: 0;
+        margin-right: 5px;
 
-        .radio-control {
-          width: 18px;
-          height: 18px;
-          position: relative;
-          display: block;
-          border-radius: 50%;
-          cursor: pointer;
-          flex-shrink: 0;
-          margin-right: 5px;
-
-          .radio-border {
-            border: 2px solid rgb(200, 200, 200);
-            background: transparent;
-          }
-
-          .radio-border,
-          .radio-checked {
-            width: 100%;
-            height: 100%;
-            position: absolute;
-            left: 0;
-            transition: all 0.25s ease;
-            top: 0;
-            border-radius: 50%;
-          }
-
-          .radio-checked {
-            background: rgba($primary, 1);
-            box-shadow: 0 3px 12px 0 rgba($primary, 0.4);
-            transform: scale(0.1);
-            opacity: 0;
-          }
+        .radio-border {
+          border: 2px solid rgb(200, 200, 200);
+          background: transparent;
         }
 
-        input {
-          opacity: 0;
+        .radio-border,
+        .radio-checked {
+          width: 100%;
+          height: 100%;
           position: absolute;
-          width: 20px;
           left: 0;
-          height: 0;
+          transition: all 0.25s ease;
+          top: 0;
+          border-radius: 50%;
+        }
 
-          &:checked {
-            & + .radio-control {
-              cursor: default;
+        .radio-checked {
+          background: rgba($primary, 1);
+          box-shadow: 0 3px 12px 0 rgba($primary, 0.4);
+          transform: scale(0.1);
+          opacity: 0;
+        }
+      }
 
-              .radio-border {
-                opacity: 0;
-                transform: scale(0.3);
-              }
+      input {
+        opacity: 0;
+        position: absolute;
+        width: 20px;
+        left: 0;
+        height: 0;
 
-              .radio-checked {
-                transform: scale(1);
-                opacity: 1;
-              }
+        &:checked {
+          & + .radio-control {
+            cursor: default;
+
+            .radio-border {
+              opacity: 0;
+              transform: scale(0.3);
+            }
+
+            .radio-checked {
+              transform: scale(1);
+              opacity: 1;
             }
           }
         }
